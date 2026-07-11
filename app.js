430ef7ca5c0004e68084a479827d1a8cf4249c7b
2EZ4BLINK <blinkcode24@gmail.com>
2026-07-11 22:00:54 +0800

    updat: setup the pool and connections

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 import express from "express";
+import dotenv from "dotenv";
+import "./config/db.js";
+import employeeRoutes from "./routes/employeeRoutes.js";
+
+dotenv.config();
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT;
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+app.use(employeeRoutes);
 
 app.listen(PORT, () => {
-  console.log("Server is running on port: ", PORT);
+  console.log(`Server is running on port ${PORT}`);
 });
---
+++ config/db.js
@@ -0,0 +1,30 @@
+import mysql from "mysql2";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+// getConnection: aks the pool if it can borrow one of the database connections.
+// Its purpose is simply to verify that:
+// MySQL is running.
+// The credentials are correct.
+// The database exists.
+// The application can connect.
+pool.getConnection((err, connection) => {
+  if (err) {
+    console.error("Database connection failed: ", err.message);
+    return;
+  }
+
+  console.log("✅ Connected to MySQL");
+
+  connection.release();
+});
+
+export default pool;
