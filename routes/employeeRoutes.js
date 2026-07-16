5be89990998a0de0cc02cd320e08de1ef8f26815
2EZ4BLINK <blinkcode24@gmail.com>
2026-07-16 12:28:46 +0800

    update: added validation

--- routes/employeeRoutes.js
+++ routes/employeeRoutes.js
@@ -12,7 +12,6 @@ const router = express.Router();
 router.get("/", (req, res) => {
   res.send("<h1>Hello fucking world</h1>");
 });
-
 router.get("/employees", fetchEmployees);
 router.get("/employees/:id", fetchEmployeeById);
 
