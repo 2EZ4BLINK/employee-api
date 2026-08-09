693de606b31cd2b9d87f438671370697f61b9d39
2EZ4BLINK <blinkcode24@gmail.com>
2026-08-09 21:20:31 +0800

    feat: add login request validation

--- middleware/validateAuth.js
+++ middleware/validateAuth.js
@@ -35,3 +35,34 @@ export const validateSignup = (req, res, next) => {
 
   next();
 };
+
+export const validateLogin = (req, res, next) => {
+  const { email, password } = req.body;
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  const trimmedEmail = email?.trim();
+
+  req.body.email = trimmedEmail;
+
+  if (!trimmedEmail)
+    return res.status(400).json({
+      message: "Email is required",
+    });
+
+  if (!emailRegex.test(trimmedEmail)) {
+    return res.status(400).json({
+      message: "Invalid email format.",
+    });
+  }
+
+  if (!password)
+    return res.status(400).json({
+      message: "Password is required",
+    });
+
+  if (password.length < 8)
+    return res.status(400).json({
+      message: "Password must be at least 8 characters",
+    });
+
+  next();
+};
--- routes/authroutes.js
+++ routes/authroutes.js
@@ -1,11 +1,11 @@
 import express from "express";
 import { loginUser, postUser } from "../controllers/authController.js";
 import { fetchEmployeeById } from "../controllers/employeeController.js";
-import { validateSignup } from "../middleware/validateAuth.js";
+import { validateLogin, validateSignup } from "../middleware/validateAuth.js";
 
 const router = express.Router();
 
 router.post("/signup", validateSignup, postUser);
-router.post("/login", loginUser);
+router.post("/login", validateLogin, loginUser);
 
 export default router;
