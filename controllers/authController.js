ac6435048c61365fed2e8ebf72a4707980f1e7db
2EZ4BLINK <blinkcode24@gmail.com>
2026-08-17 23:25:56 +0800

    fix: prevent user enumeration during login

--- controllers/authController.js
+++ controllers/authController.js
@@ -54,7 +54,7 @@ const loginUser = async (req, res, next) => {
 
     if (!user) {
       return res.status(404).json({
-        message: "User not found",
+        message: "Invalid email or password",
       });
     }
 
